ed6234e6b9e9f94d2b0eae5656f5696a84a077d1
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-08-30 19:46:17 +0300

    feat: add hook useMediaQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useMediaQuery } from "./useMediaQuery";
 
 function App() {
+  const isMobile = useMediaQuery("(max-width: 500px)");
+  const isTablet = useMediaQuery("(min-width: 501px && max-width: 1023px)");
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Типичный веб разработчик</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React Custom Hook useMediaQuery
-        </a>
+        <img src={logo} className="App-logo" alt="" />
+        {isMobile && <p>Mobile</p>}
+        {isTablet && <p>Tablet</p>}
+        {isDesktop && <p>Desktop</p>}
       </header>
     </div>
   );
